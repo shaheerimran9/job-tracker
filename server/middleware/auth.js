2ba8df02e58dbef41dae8ea90dd1510018c45185
Shaheer Imran <shaheerimran@mac.mynetworksettings.com>
2026-02-19 15:42:40 -0500

    Add auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const auth = async (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+        const error = new Error('Unauthorized: No token provided');
+        error.status = 401;
+        throw error;
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decodedToken.id).select('-password');
+
+    if (!user) {
+        const error = new Error('Unauthorized: User not found');
+        error.status = 401;
+        throw error;
+    }
+
+    req.user = user;
+    next();
+};
+
+module.exports = auth;
