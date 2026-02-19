8f073e21f13be3be99f4352b9bd0c6cdd00f4b7e
Shaheer Imran <shaheerimran@mac.mynetworksettings.com>
2026-02-19 13:52:39 -0500

    Add User Model

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    }
+}, { timestamps: true });
+
+userSchema.pre('save', async function() {
+    if(!this.isModified('password')) return;
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
