60ce1d1c79b27b27baf6354d7243e3c3063383cb
Shaheer Imran <shaheerimran@mac.mynetworksettings.com>
2026-02-19 14:07:08 -0500

    Add error handling in app.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+const errorHandler = require('./middleware/errorHandler');
+const notFound = require('./middleware/notFound');
+
 app.get('/', (req, res) => {
     res.send('Test');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
