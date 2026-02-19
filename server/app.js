ff753bbc570f674a0f5def69e289ee8e6e3fb81c
Shaheer Imran <shaheerimran@mac.mynetworksettings.com>
2026-02-19 15:14:03 -0500

    Add routing for auth

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+require('express-async-errors');
 
+app.use(express.json());
+
+const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const notFound = require('./middleware/notFound');
 
-app.get('/', (req, res) => {
-    res.send('Test');
-});
+app.use('/api/v1/auth', authRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
