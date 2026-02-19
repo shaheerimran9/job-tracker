8836d0045b7fa099296c5b3cfe603b979f876cd4
Shaheer Imran <shaheerimran@mac.mynetworksettings.com>
2026-02-19 12:42:54 -0500

    Refactor to include only server connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const app = express();
-
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
+const app = require('./app');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('Test');
-});
+const startServer = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => console.log(`Sever running on PORT ${PORT}`))
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
 
-app.listen(PORT, () => console.log(`App is running on PORT ${PORT}`));
+startServer();
+
+
