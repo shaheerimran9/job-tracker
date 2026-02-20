89de0da358374829796096f37dac478139cdfa30
Shaheer Imran <shaheerimran@Shaheers-MacBook-Air.local>
2026-02-20 14:32:51 -0500

    Add cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 require('express-async-errors');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 
 const authRoutes = require('./routes/authRoutes');
 const jobRoutes = require('./routes/jobRoutes');
