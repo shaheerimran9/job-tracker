bc63e075e13e210ca04a33760600e15388277d99
Shaheer Imran <shaheerimran@mac.mynetworksettings.com>
2026-02-19 19:08:05 -0500

    Add job route to app.js

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 require('express-async-errors');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
+app.use(cookieParser());
 
 const authRoutes = require('./routes/authRoutes');
+const jobRoutes = require('./routes/jobRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const notFound = require('./middleware/notFound');
+const auth = require('./middleware/auth');
 
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/jobs', auth, jobRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
