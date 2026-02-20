bb1fe845905b3479a099c236bf7a9b0fe8577d03
Shaheer Imran <shaheerimran@Shaheers-MacBook-Air.local>
2026-02-20 16:15:26 -0500

    Add routes to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 const App = () => {
+  const { user } = useAuth();
+
   return (
-    <h1>App</h1>
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/dashboard' element={
+        <ProtectedRoute>
+          <DashboardPage />
+        </ProtectedRoute>
+      } />
+      <Route path='/' element={<Navigate to={user ? '/dashboard' : '/login'} />} />
+    </Routes>
   );
 }
- 
+
 export default App;
